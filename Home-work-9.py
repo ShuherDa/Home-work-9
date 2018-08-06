ccab34d7f67e67e35a0ebe994471474c2a258a43
Zarubin Alexandr <shuherda@gmail.com>
2018-08-06 19:44:55 +0600

    Корректировка

--- Home-work-9.py
+++ Home-work-9.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlencode
 import requests
-import pprint as pprint
+from pprint import pprint
 
 app_id = 6652248
 auth_url = 'https://oauth.vk.com/authorize'
@@ -12,7 +12,8 @@ auth_data = {
     'response_type': 'token',
     'v': 5.80
 }
-access_token = 'd6e5eb4ff52ac19df4a55c314f3bfae227d12ed5636e4fff20f97f374627d822c9ffe66ea0a51ec402a59'
+# access_token = 'd6e5eb4ff52ac19df4a55c314f3bfae227d12ed5636e4fff20f97f374627d822c9ffe66ea0a51ec402a59'
+access_token= '3a12ff211ab8b2b8eb98a14700f4db7ee9cde3d6af528ee32a7b147ce14eb825cc95330d09ea96b935e73'
 # print('?'.join((auth_url, urlencode(auth_data))))
 
 def get_user_info(id):
@@ -30,6 +31,34 @@ class User():
         self.first_name = first_name
         self.last_name = last_name
         self.token = token
+        self.url = 'vk.com/id{}'.format(id)
+
+    def __repr__(self):
+        return self.url
+
+    def __and__(self, other):
+        response = requests.get('https://api.vk.com/method/friends.getMutual',
+                                    params=dict(
+                                    source_uid=self.id,
+                                    target_uids=other,
+                                    access_token=self.token,
+                                    v=5.80
+                                ))
+        our_friends = list()
+        friends = response.json()['response']
+        for result in friends:
+            if 'common_friends' in result:
+                for i in result['common_friends']:
+                    user_info = get_user_info(i)
+                    new_class = User(
+                        id=user_info['response'][0]['id'],
+                        first_name=user_info['response'][0]['first_name'],
+                        last_name=user_info['response'][0]['last_name'],
+                        token=access_token
+                    )
+                    our_friends.append(new_class)
+        return our_friends
+
 
     def get_friends(self):
         response = requests.get('https://api.vk.com/method/friends.get',
@@ -66,19 +95,5 @@ my_friends = []
 if 'items' in my_user.get_friends().json()['response']:
     my_friends = my_user.get_friends().json()['response']['items']
 
-pprint.pprint(get_user_info(my_friends[0]))
+pprint(my_user & my_friends)
 
-our_friends = list()
-friends = my_user.friends_mutual(my_user.id, my_friends).json()['response']
-if 'items' in friends:
-    for i in friends['items']:
-        user_info = get_user_info(i)
-        new_class = User(
-                id=user_info['response'][0]['id'],
-                first_name=user_info['response'][0]['first_name'],
-                last_name=user_info['response'][0]['last_name'],
-                token=access_token
-            )
-        our_friends.append(new_class)
-
-print(our_friends)
